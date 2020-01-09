ed075ddffe2e79b89236764cc9399825e7376051
Rick Brown <ricky.brown.00@gmail.com>
2020-01-09 00:19:59 +0000

    cannot work icons?

--- App.js
+++ App.js
@@ -1,106 +1,60 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React from 'react'
+import { Button, colors, ThemeProvider, Header } from 'react-native-elements'
+import { Text, View, StyleSheet, SafeAreaView } from 'react-native'
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const theme = {
+  colors: {
+    ...Platform.select({
+      default: colors.platform.android,
+      ios: colors.platform.ios,
+    }),
+  },
+}
 
-const App: () => React$Node = () => {
+const App = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView style={styles.container}>
+        <Header
+          placement="left"
+          // leftComponent={{ icon: 'menu', color: '#fff' }}
+          centerComponent={{
+            text: 'iOS CryptoCurrency Tracker',
+            style: { color: '#fff' },
+          }}
+          // rightComponent={{ icon: 'home', color: '#fff' }}
+        />
+        <View style={styles.buttonContainer}>
+          <Button style={styles.button} title="Hey!" />
+        </View>
       </SafeAreaView>
-    </>
-  );
-};
+    </ThemeProvider>
+  )
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  textContainer: {
+    display: 'flex',
+    justifyContent: 'center',
   },
-  body: {
-    backgroundColor: Colors.white,
+  text: {
+    textAlign: 'center',
+    padding: 10,
+    fontSize: 22,
+    fontWeight: 'bold',
+    backgroundColor: '#000ddd',
+    color: '#fff',
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  buttonContainer: {
+    display: 'flex',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  button: {
+    width: 250,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+})
 
-export default App;
+export default App
